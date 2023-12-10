d63a0825d922d390b53415a31cd3a66703ae4741
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-10 14:40:40 +0100

    fixed bug with wrong naming

--- fITness_backend/fITness_backend/wsgi.py
+++ fITness_backend/fITness_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fITness_frontend project.
+WSGI config for fITness_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fITness_frontend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fITness_backend.settings')
 
 application = get_wsgi_application()
