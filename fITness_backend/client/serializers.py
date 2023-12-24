b29096b6566146b80b4a0076a9c096c5707d50c3
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-24 11:48:09 +0100

    fix serializer

--- fITness_backend/client/serializers.py
+++ fITness_backend/client/serializers.py
@@ -4,5 +4,6 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('user_id', 'user_name', 'email', 'password', 'access_rights', 'name', 'second_name', 'surname', 'gender', 'subscription_plan', 'subscription_expiration')
+        model = Users
+        fields = ('user_id', 'user_name', 'email', 'password', 'access_rights', 'name', 'second_name', 'surname',
+                  'gender', 'subscription_plan', 'subscription_expiration')
