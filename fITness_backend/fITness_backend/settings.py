d564d84660a727919d0349c97025a385314c2ee0
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-24 11:48:54 +0100

    added rest_framework app in app's settings

--- fITness_backend/fITness_backend/settings.py
+++ fITness_backend/fITness_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'client'
+    'client',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
