1e9585f6a14c896c11087b30d3dc8e9f40710463
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-28 11:19:39 +0100

    post request for adding new user

--- fITness_backend/client/views.py
+++ fITness_backend/client/views.py
@@ -25,3 +25,10 @@ class UserApiView(APIView):
             books = Users.objects.all()
         serializer = UserSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
