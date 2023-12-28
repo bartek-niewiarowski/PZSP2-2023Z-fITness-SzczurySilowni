f48e445d41008f6713270b3c92e4373824e8fc54
Kacper Murygin <kacper.mur123@gmail.com>
2023-12-28 10:53:47 +0100

    api for getting user by email and password works

--- fITness_backend/client/views.py
+++ fITness_backend/client/views.py
@@ -8,11 +8,20 @@ from .serializers import UserSerializer
 
 
 # Create your views here.
+# class UserApiView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         users = Users.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class UserApiView(APIView):
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the todo items for given requested user
-        '''
-        users = Users.objects.all()
-        serializer = UserSerializer(users, many=True)
+    def get(self, request, format=None):
+        email = request.query_params.get('email', None)
+        password = request.query_params.get('password', None)
+        if email and password:
+            books = Users.objects.filter(email=email, password=password)
+        else:
+            books = Users.objects.all()
+        serializer = UserSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
