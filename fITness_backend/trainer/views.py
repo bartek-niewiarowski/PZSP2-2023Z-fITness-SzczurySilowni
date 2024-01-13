41f06a23abe0202acba0df7c4f22a11deea7e186
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-13 13:24:17 +0100

    implemented endpoint for getting training for a user

--- fITness_backend/trainer/views.py
+++ fITness_backend/trainer/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Appointments
+from .serializers import AppointmentsSerializer
+
+
+class AppointmentsForClientView(APIView):
+    def get(self, request, format=None):
+        client_id = request.query_params.get('client', None)
+        date = request.query_params.get('date', None)
+        if client_id and date:
+            appointments = Appointments.objects.filter(planned_start__startswith=date, client=client_id)
+        else:
+            appointments = Appointments.objects.all()
+        serializer = AppointmentsSerializer(appointments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
