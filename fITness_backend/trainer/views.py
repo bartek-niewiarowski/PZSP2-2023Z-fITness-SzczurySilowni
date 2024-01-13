91b6510c9507fe337e3e056ee2aa13172f3c5cf7
Kacper Murygin <kacper.mur123@gmail.com>
2024-01-13 13:24:25 +0100

    implemented endpoint for getting training for a trainer

--- fITness_backend/trainer/views.py
+++ fITness_backend/trainer/views.py
@@ -19,3 +19,15 @@ class AppointmentsForClientView(APIView):
             appointments = Appointments.objects.all()
         serializer = AppointmentsSerializer(appointments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AppointmentsForTrainerView(APIView):
+    def get(self, request, format=None):
+        trainer_id = request.query_params.get('trainer', None)
+        date = request.query_params.get('date', None)
+        if trainer_id and date:
+            appointments = Appointments.objects.filter(planned_start__startswith=date, trainer=trainer_id)
+        else:
+            appointments = Appointments.objects.all()
+        serializer = AppointmentsSerializer(appointments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
